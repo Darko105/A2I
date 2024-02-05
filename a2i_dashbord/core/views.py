38fddccdfb1619e722d3dd84bdb5960ac7857c6e
Darko105 <chrifsabri700@gmail.com>
2024-02-05 05:45:16 +0200

    search functon - search template - serach model

--- a2i_dashbord/core/views.py
+++ a2i_dashbord/core/views.py
@@ -4,20 +4,23 @@ from userauths.models import User
 # Create your views here.
 def index(request):
     
-    if request.user.is_staff or request.user.is_superuser:
-        admin = True
-    else:
-        admin = False
-    
     if request.user.is_authenticated:
-        name = request.user.username
-        bio = request.user.bio
+        if request.user.is_staff or request.user.is_superuser:
+            admin = True
+        else:
+            admin = False
         
+        if request.user.is_authenticated:
+            name = request.user.username
+            bio = request.user.bio
+            
+            
+        context = {
+            'userIsAdmin' : admin,
+            'username' : name,
+            'bio' : bio
+        }
         
-    context = {
-        'userIsAdmin' : admin,
-        'username' : name,
-        'bio' : bio
-    }
-    
-    return render(request,'core/index.html',context)
+        return render(request,'core/index.html',context)
+    else:
+        return render(request,'core/index.html',{})
--- a2i_dashbord/storage/views.py
+++ a2i_dashbord/storage/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from .models import Product
+from django.db.models import Q
 
 # Create your views here.
 
 
 def storage_view(request):
     
-    context = {
-        
-    }
+    if request.method == 'POST':
+        searched = request.POST['searchProd']
+        product = Product.objects.filter(
+            Q(name__contains = searched)|
+            Q(discription__contains = searched))
+        print(product)
+        oddEven = 1
     
-    return render(request,'storage/storage.html',context)
+        context = {
+                'product' : product,
+                'oddEven' :oddEven
+        }
+        return render(request,'storage/storage.html',context)
+    else:
+        product = Product.objects.all()
+        return render(request,'storage/storage.html',{'empty':product})
+    
+    
